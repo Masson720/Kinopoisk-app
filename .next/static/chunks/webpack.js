29d63ed321795c877a093ebc524f50f1638d83b7
Masson720 <Maestro.2@mail.ru>
2022-08-09 01:46:49 +0300

    Logic & API / Test

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "d0e1183147d7b1a7"; }
+/******/ 		__webpack_require__.h = function() { return "6756b0707b92a374"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
